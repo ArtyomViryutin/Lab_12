a04d2d09acd484c3b2b7d1ea1b8512be24e87276
ArtyomViryutin <48865956+ArtyomViryutin@users.noreply.github.com>
2019-06-04 21:23:01 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,20 +8,29 @@ int main() {
 		Create_Textfile(); // Создание и заполнение текстового файла
 		root = Create_Tree(); // Создание дерева
 	}
-	printf("Tree in default order:\n");
-	Print_Tree(root);// Вывод дерева при инфиксном обходе
-	printf("\n");
-	printf("Tree in reverse order:\n"); 
-	Print_Reverse_Tree(root); // Вывод дерева при постфиксном обходе
+	printf("Tree in the default order:\n");
+	Print_Tree(root);// Вывод элементов дерева по возрастанию
 	printf("\n");
 	printf("------------------------------------------------\n");
+	printf("Tree in the reverse order:\n"); 
+	Print_Reverse_Tree(root); // Вывод элементов дерева по убыванию
+	printf("\n");
+	printf("------------------------------------------------\n");
+	printf("Tree in the straight order:\n");
+	Print_Straight(root);
+	printf("\n");
+	printf("------------------------------------------------\n");
+	printf("Tree in the wide order:\n");
 	WidePrint(root); // Вывод дерева при обходе в ширину
 	printf("------------------------------------------------\n\n");
-	N = Input_N(); // Ввод уровня, на котором нужно посчитать количество слов
+	while ((N = Input_N()) == 0) // Ввод уровня, на котором нужно посчитать количество слов
+		N = Input_N;
+	
 	Task(root, 1, N, &rec); // Вызов рекурентной функции задания
 	printf("At the %d level - %d word(s).\n", N, rec); // Вывод количества слов на уровне N
 	Free_Tree(root); // Вызов функции очистки узлов дерева
 	system("pause");
 	return 0;
 }
+
 
